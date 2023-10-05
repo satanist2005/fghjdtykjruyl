7c76e8a9803b5833627ec3c1e486494dec81c024
Satanist <82795576+satanist2005@users.noreply.github.com>
2023-10-05 14:59:54 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,86 @@
+a = int(input())
+b = int(input())
+if a < b:
+    print(a)
+else:
+    print(b)
+
+
+x = int(input(''))
+if x > 0:
+    print('sign(x)=1')
+elif x < 0:
+    print('sign(x)=-1')
+else:
+    print('sign(x)=0')
+
+
+year = int(input())
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+    print('YES')
+else:
+    print('NO')
+
+
+year = int(input())
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+    print('YES')
+else:
+    print('NO')
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+    print(3)
+elif a == b or b == c or a == c:
+    print(2)
+else:
+    print(0)
+
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if x1 == x2 or y1 == y2:
+    print('YES')
+else:
+    print('NO')
+
+
+n = int(input())
+m = int(input())
+k = int(input())
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print('YES')
+else:
+    print('NO')
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+    print(3)
+elif a == b or b == c or a == c:
+    print(2)
+else:
+    print(0)
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if x1 == x2 or y1 == y2:
+    print('YES')
+else:
+    print('NO')
+n = int(input())
+m = int(input())
+k = int(input())
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print('YES')
+else:
+    print('NO')
+
